76e546fc886f163a1b2fc10d518e3c93f0bb3ceb
prathap <mail2bprathap@gmail.com>
2017-09-13 21:28:17 -0400

    addding basic python script

---
+++ basic.py
@@ -0,0 +1,17 @@
+n1 = 4
+n2 = 2
+###addition of two numbers
+N = n1+n2
+print(N)
+###subtraction of two numbers
+sub = n1-n2
+print(sub)
+###dicison of two numbers
+div = n1/n2
+print(div)
+##modular of two numbers
+mod = n1%n2
+print(mod)
+####exponential of two numbers
+exp = n1**n2
+print(exp)
