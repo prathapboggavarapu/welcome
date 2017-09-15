a997d9dd28728f18bdb9ca4704a3c37671ba9249
prathap <mail2bprathap@gmail.com>
2017-09-15 00:13:14 -0400

    adding new python files

---
+++ diff.py
@@ -0,0 +1,14 @@
+###please enter the value
+
+x=int(raw_input("please enter the value: "))
+
+if x>17:
+    result = 2*(x-17)
+    print(result)
+else:
+    result = 17 - x
+    print(result)
+
+
+
+
---
+++ operators.py
@@ -0,0 +1,7 @@
+N = int(raw_input("Please enter the Number : "))
+if N == 17:
+    print "The difference between 17 and Given number(N) is ZERO"
+elif N > 17:
+    print ​abs​(N-17)
+elif N < 17:
+    print abs(17-N)
